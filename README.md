21aaebfbaea929c01433b31b54971e28212745eb
Matthew Segal <mattdsegal@gmail.com>
2018-07-07 18:45:03 +1000

    view works

--- README.md
+++ README.md
@@ -13,3 +13,58 @@ Issues
 
 * fox jumps through trees occasionally
 * tree maze is too random
+
+# To do
+
+view layer
+
+- make canvas
+- draw trees
+- draw fox
+- draw chicken
+- canvas resize
+
+initialize game state
+
+- pre-populate grid
+- locate fox
+- locate chicken
+
+create API for the fox
+
+- fox policy is random
+- get available actions
+- get game board
+- get chicken location
+
+provide API for the chicken
+
+- chicken policy is random
+- get available actions
+- get game board
+- get fox location
+
+create fox policy
+
+- random policy
+- A* pathing
+
+create chicken policy
+
+- random policy
+- policy iteration
+- value iteration
+- monte carlo
+- temporal difference
+- TD(lambda)
+- player input policy (?) do this last
+
+create visualizations
+
+- chicken value function
+- fox value functions
+
+create about page
+
+- about page modal for the entire site
+- about page modal for each algorithm
--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-'use strict'
 import C from 'constants'
-import Actor from 'actor'
-import GameBoard from 'gameboard'
-import View from 'view'
-import {onKeyDown, FoxLoop} from 'events'
 
-// Initialise game board and view
-const gameboard = new GameBoard()
-const player = new Actor(C.PLAYER, gameboard)
-const fox = new Actor(C.FOX, gameboard)
-const view = new View(gameboard)
-const foxLoop = new FoxLoop(gameboard, fox, player, view)
-view.render()
+import View from './view'
 
-// Hook up events
-window.addEventListener('resize', view.render, false);
-document.addEventListener('keydown', onKeyDown(player, view))
-setInterval(() => foxLoop.run(), C.FOX_TICK)  // Start the hunt!
+
+// Initialize grid
+const fillCol = () => Math.random() > 0.2 ? C.EMPTY : C.TREE
+const fillRow = () => Array(C.BOARD_LENGTH).fill(0).map(fillCol)
+const grid = Array(C.BOARD_LENGTH).fill(0).map(fillRow)
+
+// Draw grid
+View.onImagesLoaded().then(() => View.drawGrid(grid))
+
